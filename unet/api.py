bcf05fbbc8a5fd673ee3f72d9d43e50b0cf6e6f2
adnaneds <adnane.aitoussayer@etud.univ-angers.fr>
2022-07-03 13:29:42 +0200

    update_0

--- unet/api.py
+++ unet/api.py
@@ -22,14 +22,15 @@ To start populating this file, take a look at the docs [1] and at a canonical ex
 """
 
 from functools import wraps
+import shutil
+import tempfile
 
 from aiohttp.web import HTTPBadRequest
+from webargs import fields, validate
 
 
 def _catch_error(f):
-    """
-    Decorate function to return an error as HTTPBadRequest,
-    in case it fails.
+    """Decorate function to return an error as HTTPBadRequest, in case
     """
     @wraps(f)
     def wrap(*args, **kwargs):
@@ -39,6 +40,97 @@ def _catch_error(f):
             raise HTTPBadRequest(reason=e)
     return wrap
 
+
+def get_metadata():
+    metadata = {
+        "author": "Adnane"
+    }
+    return metadata
+
+
+def get_predict_args():
+    """
+    Input fields for the user.
+    """
+    arg_dict = {
+        "demo-image": fields.Field(
+            required=False,
+            type="file",
+            location="form",
+            description="image",  # needed to be parsed by UI
+        ),
+        # Add format type of the response of predict()
+        # For demo purposes, we allow the user to receive back
+        # either an image or a zip containing an image.
+        # More options for MIME types: https://mimeapplication.net/
+        "accept": fields.Str(
+            description="Media type(s) that is/are acceptable for the response.",
+            validate=validate.OneOf(["image/*", "application/zip"]),
+        ),
+    }
+    return arg_dict
+
+
+import numpy as np
+import os
+import cv2
+import matplotlib.pyplot as plt
+import skimage.io as io
+import cv2
+from skimage import transform
+from skimage import img_as_bool
+
+
+
+IMG_WIDTH = 512
+IMG_HEIGHT = 512
+IMG_CHANNELS = 3
+
+input_size_2 = (IMG_WIDTH, IMG_HEIGHT)
+input_size_3 = (IMG_WIDTH,IMG_HEIGHT,IMG_CHANNELS)
+
+
+
+@_catch_error
+def predict(**kwargs):
+    """
+    Return same inputs as provided.
+    """
+    filepath = kwargs['demo-image'].filename
+
+    # Return the image directly
+    if kwargs['accept'] == 'image/*':
+        
+        convert_gray = cv2.imread(filepath, cv2.IMREAD_GRAYSCALE)
+        convert_gray = np.asarray(convert_gray, dtype="uint8")
+        mask_resized = cv2.resize(convert_gray, input_size_2, interpolation=cv2.INTER_NEAREST)
+        
+        return mask_resized
+        #return open(filepath, 'rb')
+
+
+
+
+
+
+    # Return a zip
+    elif kwargs['accept'] == 'application/zip':
+
+        zip_dir = tempfile.TemporaryDirectory()
+
+        # Add original image to output zip
+        shutil.copyfile(filepath,
+                        zip_dir.name + '/demo.png')
+
+        # Add for example a demo txt file
+        with open(f'{zip_dir.name}/demo.txt', 'w') as f:
+            f.write('Add here any additional information!')
+
+        # Pack dir into zip and return it
+        shutil.make_archive(zip_dir.name, format='zip', root_dir=zip_dir.name)
+        zip_path = zip_dir.name + '.zip'
+
+        return open(zip_path, 'rb')
 
 # def get_metadata():
 #     return {}
